42614753fca02bf38a8e80a03647a66e2b32187c
Phongraphi Ruamtham <avetavos@hotmail.com>
2023-08-14 13:30:45 +0700

    feat: add audio resource

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use rusty_audio::Audio;
+use std::error::Error;
+
+fn main() -> Result <(), Box<dyn Error>>  {
+    let mut audio = Audio::new();
+    audio.add("explode", "./audio/original/explode.wav");
+    audio.add("lose", "./audio/original/lose.wav");
+    audio.add("move", "./audio/original/move.wav");
+    audio.add("pew", "./audio/original/pew.wav");
+    audio.add("startup", "./audio/original/startup.wav");
+    audio.add("win", "./audio/original/win.wav");
+
+    audio.play("startup");
+
+    audio.wait();
+    Ok(())
 }
